ebf758d72f8ae9361019db13d01d1fffcb261581
Dinesh Santosh Khairnar <96805747+dskhairnar@users.noreply.github.com>
2026-08-07 17:14:25 +0530

    fix(config): load environment variables from default location instead of specific file

--- internal/config/config.go
+++ internal/config/config.go
@@ -891,7 +891,7 @@ func NewConfig() (*Configuration, error) {
 	v := viper.New()
 
 	// Step 1: Load `.env` then `.env.local` if they exist.
-	_ = godotenv.Load(".env")
+	_ = godotenv.Load()
 
 	// Step 2: Initialize Viper
 	v.SetConfigName("config")
