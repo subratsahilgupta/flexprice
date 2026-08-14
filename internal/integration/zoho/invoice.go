6abb7eeefecdcf87a128233b8670214e1c5b5eeb
tsage <subratsahilgupta@gmail.com>
2026-08-14 23:53:43 +0530

    Revert "refactor(zoho): enhance tax handling in invoice and item creation processes"
    This reverts commit a47a20cf2e23cafe7d533a32521f82d1aa6066c5.

--- internal/integration/zoho/invoice.go
+++ internal/integration/zoho/invoice.go
@@ -324,22 +324,16 @@ func (s *InvoiceService) buildLineItems(ctx context.Context, flexInvoice *invoic
 		qty, rate := s.normalizeRateAndQuantity(li, settings, flexInvoice.BillingPeriod)
 		name := lo.FromPtrOr(li.DisplayName, "Charge")
 		childName := childCustomerIDToName[lineItemIDToChildCustomer[li.ID]]
-		lineItem := InvoiceLineItem{
+		out = append(out, InvoiceLineItem{
 			Name:        name,
 			Description: formatPeriodDescription(childName, li.PeriodStart, li.PeriodEnd),
 			Quantity:    qty,
 			Rate:        rate,
 			Discount:    li.LineItemDiscount.Add(li.InvoiceLevelDiscount),
 			ItemID:      priceToItemID[lo.FromPtr(li.PriceID)],
-		}
-		if taxRes != nil {
-			if taxRes.IsTaxable {
-				lineItem.TaxID = taxRes.TaxID
-			} else {
-				lineItem.TaxExemptionID = taxRes.TaxExemptionID
-			}
-		}
-		out = append(out, lineItem)
+			//TaxID:          taxRes.TaxID,
+			//TaxExemptionID: taxRes.TaxExemptionID,
+		})
 	}
 	return out, nil
 }
