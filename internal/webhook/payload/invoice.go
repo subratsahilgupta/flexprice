e774c0fa28e467a15d2653c01dba4d98f9dee67e
omkar273 <omkarsonawane622@gmail.com>
2026-08-17 18:43:23 +0530

    fix(invoice): return complete invoice object in webhook

--- internal/webhook/payload/invoice.go
+++ internal/webhook/payload/invoice.go
@@ -67,8 +67,8 @@ func (b *InvoicePayloadBuilder) BuildPayload(ctx context.Context, eventType type
 	}
 	invoice.InvoicePDFURL = lo.ToPtr(pdfUrl)
 
-	payload := webhookDto.NewInvoiceWebhookPayload(invoice, eventType)
+	// payload := webhookDto.NewInvoiceWebhookPayload(invoice, eventType)
 
 	// Return the invoice response as is
-	return json.Marshal(payload)
+	return json.Marshal(invoice)
 }
